0a7ee5f7ff9a48e7e954a0d2f14d357739d7a34b
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2021-02-26 00:21:01 +0000

    Added basic form styling

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import { useState } from 'react';
 
 const AddTask = () => {
+  const [text, setText] = useState('');
+  const [day, setDay] = useState('');
+  const [reminder, setReminder] = useState(false);
+
   return (
     <form className='add-form'>
       <div className='form-control'>
-        <labe>Task</labe>
-        <input type='text' placeholder='Add Task' />
+        <label>Task</label>
+        <input
+          type='text'
+          placeholder='Add Task'
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
       </div>
       <div className='form-control'>
-        <labe>Day & Time</labe>
-        <input type='text' placeholder='Add Day & Time' />
+        <label>Day & Time</label>
+        <input
+          type='text'
+          placeholder='Add Day & Time'
+          value={day}
+          onChange={(e) => setDay(e.target.value)}
+        />
       </div>
       <div className='form-control form-control-check'>
-        <labe>Set Reminder</labe>
-        <input type='checkbox' />
+        <label>Set Reminder</label>
+        <input
+          type='checkbox'
+          value={reminder}
+          onChange={(e) => setReminder(e.currentTarget.checked)}
+        />
       </div>
       <input type='submit' value='save task' className='btn btn-block' />
     </form>
