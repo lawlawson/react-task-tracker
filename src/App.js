661a17e99efb7364d45d6167a317f6427f51b9e9
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2021-03-04 13:11:12 +0000

    Added functionality to add tasks to server

--- src/App.js
+++ src/App.js
@@ -23,11 +23,29 @@ const App = () => {
     return data
   }
 
+  //Fetch Task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/id`)
+    const data = await res.json()
+
+    return data
+  }
+
   //Add Task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 1000) + 1
-    const newTask = {id, ...task}
-    setTasks([...tasks, newTask])
+
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(task),
+    })
+
+      const data = await res.json()
+
+      setTasks([...tasks, data])
+
   }
 
   //Delete Task
@@ -41,6 +59,8 @@ const App = () => {
 
   //Toggle Reminder
   const toggleReminder = (id) => {
+    // const taskToToggle = await fetchTask(id)
+
     setTasks(tasks.map((task) => task.id === id ? {...task, reminder:!task.reminder } : task ))
   }
  
