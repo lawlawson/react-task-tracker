93428f20b242b39eb569c6530e0824bf293f87fa
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2021-03-04 13:53:01 +0000

    Added About and implemented routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header'
-import Footer from "./components";
+import Footer from './components/Footer'
+import About  from './components/About'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
@@ -83,11 +85,19 @@ const App = () => {
   }
  
   return (
+    <Router>
     <div className='container'>
       <Header onAdd={() => setShowAddTask(!showAddTask)}showAdd={showAddTask}/>
-      {showAddTask && <AddTask onAdd={addTask} showAdd={showAddTask}/>}
-      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>) : ('No Tasks To Show')} 
+      <Route path='/' exact render={(props) => (
+        <>
+          {showAddTask && <AddTask onAdd={addTask} showAdd={showAddTask}/>}
+          {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>) : ('No Tasks To Show')}
+        </>
+      )} />
+      <Route path='/about' component={About} />
+      <Footer />
     </div>
+    </Router>
   );
 }
 
